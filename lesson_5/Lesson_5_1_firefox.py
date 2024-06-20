a6ad7ca55c74268d69b2da23e76552c758ff5de3
SuronSkypro <suronskypro@yandex.ru>
2024-06-20 10:53:07 +0300

    lesson_5

---
+++ lesson_5/Lesson_5_1_firefox.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+
+
+browser= webdriver.Firefox()
+browser.get('https://the-internet.herokuapp.com/add_remove_elements/')
+
+
+temp__ = "button[onclick*='addElement']"
+button = browser.find_element(By.CSS_SELECTOR, temp__)
+button.click()
+button.click()
+button.click()
+button.click()
+button.click()
+button.click()
+
+
+
+buttons = browser.find_elements(By.CLASS_NAME, "added-manually")
+
+# Подсчитываем количество найденных кнопок
+count = len(buttons)
+print("Количество кнопок с классом 'added-manually':", count)
+
+print (str(len))
+
+
+time.sleep(10)
+browser.quit()
+
+
+
+
