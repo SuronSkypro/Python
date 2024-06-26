254054c670f82174291ce0f5b53a3d2c507d2f99
SuronSkypro <suronskypro@yandex.ru>
2024-06-26 10:27:38 +0300

    lesson_6

---
+++ lesson_6/lesson_6_3.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time 
+
+
+browser= webdriver.Firefox()
+
+browser.get('https://bonigarcia.dev/selenium-webdriver-java/loading-images.html')
+
+
+waiter = WebDriverWait(browser, 30)
+
+images = waiter.until(
+    EC.presence_of_element_located((By.ID, 'landscape'))
+)
+
+
+print("КартинокА появилась  ", images)
+images=browser.find_element(By.ID,"award")
+print("Атрибут - ", images.get_attribute("src"))
+
+
+browser.quit()
+
